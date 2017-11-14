21fed0480d8692e1b46a9703a14cf62752a79155
Nathan Opseth <fleetfacility@gmail.com>
2017-11-13 17:44:22 -0800

    Setup mobile navigation

--- client/main.js
+++ client/main.js
@@ -16,6 +16,7 @@ Tracker.autorun(() => {
 
 Tracker.autorun(() => {
   const selectedNoteId = Session.get('selectedNoteId');
+  Session.set('isNavOpen', false);
 
   if (selectedNoteId) {
     browserHistory.replace(`/dashboard/${selectedNoteId}`);
@@ -25,7 +26,7 @@ Tracker.autorun(() => {
 Tracker.autorun(() => {
   const isNavOpen = Session.get('isNavOpen');
 
-  document.body.classList.toggle('isNavOpen', isNavOpen);
+  document.body.classList.toggle('is-nav-open', isNavOpen);
 });
 
 Meteor.startup(() => {
